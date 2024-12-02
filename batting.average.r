68c7bbeb69241fb615b75c664195dbdc624d6e8f
dylanpearl <dpearl@usf.edu>
2024-12-02 17:36:01 -0500

    Update batting.average.r

--- batting.average.r
+++ batting.average.r
@@ -1,12 +1,4 @@
 #' Calculate Batting Average
-#'
-#' This function calculates the batting average for each player in the dataset.
-#'
-#' @param data A data frame containing baseball player statistics with columns: Player, AtBats, Hits.
-#' @return A data frame with a new column "BattingAverage" added.
-#' @examples
-#' data <- read.csv("data/baseball_data.csv")
-#' calculate_batting_average(data)
 calculate_batting_average <- function(data) {
   data$BattingAverage <- data$Hits / data$AtBats
   return(data)
